bf8e62d6a886453f86ca109e9b33e523be1d84f9
Sabrina <sabrina.conklin@vtsd.com>
2022-10-12 09:16:26 -0400

    Commit and Push

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,13 +7,40 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+        System.out.println("Lab04a, 100 Point Version\n");
 
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        double monthlyRate = (annualRate/100) / 12;
+        double numMonths = numYears * 12;
+        double numerator = monthlyRate * Math.pow((1 + monthlyRate),numMonths);
+        double denominator = Math.pow((1 + monthlyRate), numMonths) - 1;
+        double monthlyPay = (numerator/denominator) * principal;
+        double totalPayments = monthlyPay * numMonths;
+        double TotalInterest = totalPayments - principal;
 
 
+
+        monthlyPay = monthlyPay * 100;
+        monthlyPay = Math.round(monthlyPay);
+        monthlyPay = monthlyPay / 100;
+
+        totalPayments = totalPayments * 100;
+        totalPayments = Math.round(totalPayments);
+        totalPayments = totalPayments / 100;
+
+        TotalInterest = TotalInterest * 100;
+        TotalInterest = Math.round(TotalInterest);
+        TotalInterest = TotalInterest/100;
+
+
+        System.out.println("Principal:\t\t\t" + "$" + principal);
+        System.out.println("AnnualRate:\t\t\t" + annualRate + "%");
+        System.out.println("Number of Years:\t" + numYears);
+        System.out.println("Monthly Payment:\t" + "$" + monthlyPay);
+        System.out.println("Total Payments:\t\t" + "$" + totalPayments);
+        System.out.println("Total Interest: \t" + "$" + TotalInterest);
 
 
 
@@ -24,4 +51,5 @@ public class Lab04avst
         System.out.println();
     }
 }
+
 
